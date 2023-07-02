11997b62f0744386186bf0daff72e566408e9d02
Viktor <oliinyk.v.7.7@gmail.com>
2023-07-03 01:27:47 +0300

    appHeader component is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
 import AppBanner from './components/appBanner/AppBanner'
+import AppHeader from './components/appHeader/AppHeader'
 
 function App() {
 
   return (
     <>
+      <AppHeader/>
       <AppBanner/>
     </>
   )
